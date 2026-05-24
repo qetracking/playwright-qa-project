ef7f92879427973f1af7e2dcd3d87e456fde2337
qetracking <gestionall15896@gmail.com>
2026-05-23 20:57:57 -0500

    Add checkout valid purchase test

--- tests/checkout.spec.js
+++ tests/checkout.spec.js
@@ -27,3 +27,36 @@ page.locator('[data-test="error"]')
 });
 
 });
+
+
+test('Checkout valid | User completes purchase successfully', async ({ page }) => {
+
+await page.goto('https://www.saucedemo.com/');
+
+await page.fill('#user-name', 'standard_user');
+
+await page.fill('#password', 'secret_sauce');
+
+await page.click('#login-button');
+
+await page.click('text=Add to cart');
+
+await page.click('.shopping_cart_link');
+
+await page.click('#checkout');
+
+await page.fill('#first-name', 'QA');
+
+await page.fill('#last-name', 'Automation');
+
+await page.fill('#postal-code', '12345');
+
+await page.click('#continue');
+
+await page.click('#finish');
+
+await expect(
+page.locator('.complete-header')
+).toContainText('Thank you for your order!');
+
+});
